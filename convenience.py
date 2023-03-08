149c437ab1d5a638579b95616d55c0719e0d22bd
ekinugurel <ugurel@uw.edu>
2023-03-08 11:23:53 -0800

    added convenience tools

---
+++ convenience.py
@@ -0,0 +1,5 @@
+## Add reverse geocoding functions here
+
+def bounding_box(points):
+    x_coordinates, y_coordinates = zip(*points)
+    return [(min(x_coordinates), min(y_coordinates)), (max(x_coordinates), max(y_coordinates))]
