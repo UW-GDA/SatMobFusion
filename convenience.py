e1da69f4d47f5c6aff7b2ebcb9d84978640d9c87
Steffen Coenen <steffen.coenen@web.de>
2023-03-10 15:06:10 -0800

    updated convenience.py

--- convenience.py
+++ convenience.py
@@ -3,3 +3,7 @@
 def bounding_box(points):
     x_coordinates, y_coordinates = zip(*points)
     return [(min(x_coordinates), min(y_coordinates)), (max(x_coordinates), max(y_coordinates))]
+
+def get_rectangular_polygon_from_bounding_box(lon_min, lon_max, lat_min, lat_max):
+    # return [(minx,maxy),(maxx,maxy),(maxx,miny),(minx,miny),(minx,maxy)]
+    return [(lon_min,lat_max),(lon_max,lat_max),(lon_max,lat_min),(lon_min,lat_min),(lon_min,lat_max)]
